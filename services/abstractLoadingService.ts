5e24934ab9d39fb0105f9fc1a18fdce847c1a682
vosonha89 <vosonha89@gmail.com>
2023-03-30 16:56:14 +0700

    update

---
+++ services/abstractLoadingService.ts
@@ -0,0 +1,15 @@
+﻿import 'reflect-metadata';
+import { singleton } from 'tsyringe';
+
+@singleton()
+export abstract class AbstractLoadingService {
+    /**
+     * For showing loading
+     */
+    public abstract show(): boolean;
+
+    /**
+     * For hidden loading
+     */
+    public abstract hide(): boolean;
+}
