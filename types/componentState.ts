8ccf196872c5c7fb1d4114a9464b5f1aeb5214b4
vosonha89 <vosonha89@gmail.com>
2023-06-09 09:03:31 +0700

    update

--- types/componentState.ts
+++ types/componentState.ts
@@ -1,13 +1,11 @@
 import { ObjectHelper } from '../functions/objectHelper';
-import { AbstractModel, ErrorModel } from "./abstractModel";
 
 /**
  * Abstract state for implement each component
  * Tracking all state in one state. Etc: changed, rendered...
  */
-export abstract class ComponentState<TModel extends AbstractModel<ErrorModel>>{
+export abstract class ComponentState {
     public isReady = false;
-    public model!: TModel;
 
     /**
      * Deep copy current state
