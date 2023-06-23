f86b8ab49ceee3616b21a7ca994b39df56abae0e
vosonha89 <vosonha89@gmail.com>
2023-06-23 14:17:37 +0700

    add logging service

---
+++ services/loggingService.ts
@@ -0,0 +1,36 @@
+﻿import 'reflect-metadata';
+import { singleton } from 'tsyringe';
+import { ObjectHelper } from '../functions/objectHelper';
+import { AbstractLogggingService } from './abstractLogggingService';
+
+export interface ServerError {
+    code: string;
+    title: string;
+    message: string;
+    type: string;
+}
+
+@singleton()
+export class LogggingService extends AbstractLogggingService {
+    /**
+     * Log error
+     * @param ex
+     */
+    public logError(ex: unknown): void {
+        console.log(ex);
+    }
+
+    /** Get error*/
+    public getError(language: any, code: string, defaultValue: string): string {
+        try {
+            const value = ObjectHelper.getValue(language, code) as string;
+            if (value) {
+                return value;
+            }
+            return defaultValue;
+        }
+        catch (ex) {
+            return defaultValue;
+        }
+    }
+}
