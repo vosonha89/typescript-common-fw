113ef9770f241f18f0d72e24abddfd5cadb3761b
Vo Son Ha <vosonha89@gmail.com>
2024-05-03 21:27:43 +0700

    remove dataobject

--- types/abstractType.ts
+++ types/abstractType.ts
@@ -9,18 +9,3 @@ export abstract class AbstractType {
  * Any Type
  */
 export type AnyType = any;
-
-/**
- * Unknow Type with index
- */
-export class DataObjectIndex {
-    [key: string]: unknown | string | AnyType;
-
-    public getValueByName(name: string): string {
-        return this[name] as string;
-    }
-
-    public getValueByIndex(index: number): string {
-        return this[index] as string;
-    }
-}
